76a837337f5c884fbc805f8eef97aec4559cb882
Xandertron <66480163+Xandertron@users.noreply.github.com>
2020-07-05 20:12:42 -0500

    take 4

--- shapez/bundle.js
+++ shapez/bundle.js
@@ -46053,7 +46053,7 @@ if (window.coreThreadLoadedCb) {
 
 console.log(
     `%cshapez.io ️%c\n© 2020 Tobias Springer IT Solutions\nCommit %c${"59f5d75"}%c on %c${new Date(
-        1593994052375
+        1593997274238
     ).toLocaleString()}\n`,
     "font-size: 35px; font-family: Arial;font-weight: bold; padding: 10px 0;",
     "color: #aaa",
@@ -54111,7 +54111,7 @@ class SettingsState extends _core_textual_game_state__WEBPACK_IMPORTED_MODULE_0_
 
     renderBuildText() {
         const labelVersion = this.htmlElement.querySelector(".buildVersion");
-        const lastBuildMs = new Date().getTime() - 1593994052375;
+        const lastBuildMs = new Date().getTime() - 1593997274238;
         const lastBuildText = Object(_core_utils__WEBPACK_IMPORTED_MODULE_1__["formatSecondsToTimeAgo"])(lastBuildMs / 1000.0);
 
         const version = _translations__WEBPACK_IMPORTED_MODULE_3__["T"].settings.versionBadges["dev"];
